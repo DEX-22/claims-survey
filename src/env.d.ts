3c36aade249262a88b88552beb2989480fdfb952
chriscass <christiancasafrancarocha@gmail.com>
2023-06-14 11:50:02 -0500

    fix: add .env and import axios

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly BACKEND_AMG_URL: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
