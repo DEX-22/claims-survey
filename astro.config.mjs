3d981dd7f865080136fc80a857e51a4fe8208991
Enrique Saenz <enrique.saenz@alarcongroup.us>
2023-06-14 13:59:57 -0500

    validation before enter in process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,38 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output:'hybrid'
+});
+
+
+export async function getStaticPaths() {
+    const pages = [
+    {
+        slug: "survey",
+        title: "Tienda de Astro",
+        text: "¡Te damos la bienvenida a la tienda de Astro!",
+        },
+    {
+        slug: undefined,
+        title: "Tienda de Astro",
+        text: "¡Te damos la bienvenida a la tienda de Astro!",
+      },
+      {
+        slug: "products",
+        title: "Productos de Astro",
+        text: "Tenemos muchos productos para ti",
+      },
+      {
+        slug: "products/astro-handbook",
+        title: "El libro definitivo de Astro",
+        text: "Si quieres aprender sobre Astro, debes leer este libro.",
+      },
+    ];
+    return pages.map(({ slug, title, text }) => {
+      return {
+        params: { slug },
+        props: { title, text },
+      };
+    });
+  }
